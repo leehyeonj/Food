cfc8af5802d5398fe9a199f1d8d9509550ae3ee2
leehyeonj <billaa@naver.com>
2020-11-17 21:34:58 +0900

    animation

--- JFX/src/page_move/RootController.java
+++ JFX/src/page_move/RootController.java
@@ -3,13 +3,18 @@ package page_move;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public class RootController implements Initializable{
 
@@ -24,6 +29,21 @@ public class RootController implements Initializable{
 			Parent login = FXMLLoader.load(getClass().getResource("login.fxml"));
 			StackPane root = (StackPane)btnLogin.getScene().getRoot();
 			root.getChildren().add(login);
+			
+			login.setTranslateX(350);
+			
+			Timeline timeline = new Timeline();
+			KeyValue keyValue = new KeyValue(login.translateXProperty(), 0);
+			KeyFrame keyFrame = new KeyFrame(Duration.millis(100), new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					//애니메이션이 종료된 후 실행할 코드
+					
+					
+				}
+			}, keyValue);
+			timeline.getKeyFrames().add(keyFrame);
+			timeline.play();
 		} catch (Exception e2) {}
 	}
 
